7400eedfc883cee8634ab07bb577b2f36483dac7
Michael Gregoire <mdgregoire@gmail.com>
2018-01-04 18:38:44 -0600

    practising git stuff

--- lastitem.js
+++ lastitem.js
@@ -1,1 +1,6 @@
-console.log("lastItem.js");
+var fruits = ['apple', 'orange', 'banana'];
+function lastItem( lastItemArray ) {
+  var arrayLength = lastItemArray.length; //finding the length of the array
+  return lastItemArray [arrayLength -1]; //converting length to index value
+}
+console.log ('The last item in the array is', lastItem(fruits));
